7364e07fb5be4bc797bf859514f67d2a3eec3fa7
low_kb <69186337+lowkb@users.noreply.github.com>
2025-06-28 01:09:58 +0200

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 export function Loader() {
   return (
     <div class="loader-screen">
-      <div class="loader-text">Loading low_kb shell...</div>
+      <div class="loader-text">Loading...</div>
     </div>
   )
 }

d13f43ee6616713620d8cf6d31cef69e695537bc
low_kb <69186337+lowkb@users.noreply.github.com>
2025-06-28 00:54:04 +0200

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,17 @@
 import { render } from 'preact'
+import { useState, useEffect } from 'preact/hooks'
 import { Home } from './home.jsx'
+import { Loader } from './components/Loader.jsx'
 
-render(<Home />, document.getElementById('home'))
+function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setLoading(false), 2000)
+    return () => clearTimeout(timeout)
+  }, [])
+
+  return loading ? <Loader /> : <Home />
+}
+
+render(<App />, document.getElementById('home'))

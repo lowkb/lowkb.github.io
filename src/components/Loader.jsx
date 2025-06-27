0a5d52866c767af32f7e346c387de17987354df0
low_kb <69186337+lowkb@users.noreply.github.com>
2025-06-28 01:40:40 +0200

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'preact/hooks'
+
 export function Loader() {
+  const [dots, setDots] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prev) => (prev + 1) % 4)
+    }, 500)
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
-    <div class="loader-screen">
-      <div class="loader-text">Loading...</div>
+    <div className="loader">
+      <span className="loader-text">
+        {'> loading' + '.'.repeat(dots)}
+      </span>
     </div>
   )
 }

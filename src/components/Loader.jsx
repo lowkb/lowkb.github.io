e30b6b53da62ecc2efb1c58157ab620c81ef733a
low_kb <69186337+lowkb@users.noreply.github.com>
2025-06-28 01:43:24 +0200

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,21 +1,27 @@
 import { useEffect, useState } from 'preact/hooks'
 
 export function Loader() {
+  const totalSteps = 10
+  const [progress, setProgress] = useState(0)
   const [dots, setDots] = useState(0)
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setDots((prev) => (prev + 1) % 4)
-    }, 500)
+      setDots((d) => (d + 1) % 4)
+      setProgress((p) => Math.min(p + 1, totalSteps))
+    }, 300)
 
     return () => clearInterval(interval)
   }, [])
 
+  const bar = '[' + '#'.repeat(progress) + ' '.repeat(totalSteps - progress) + ']'
+
   return (
     <div className="loader">
-      <span className="loader-text">
-        {'> loading' + '.'.repeat(dots)}
-      </span>
+      <div className="loader-box">
+        <div className="loader-text">{'> loading low_kb shell' + '.'.repeat(dots)}</div>
+        <div className="loader-bar">{bar}</div>
+      </div>
     </div>
   )
 }

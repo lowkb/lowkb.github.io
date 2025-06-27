2031b10a369581d2393500debce2690b5d797cd5
low_kb <69186337+lowkb@users.noreply.github.com>
2025-06-28 00:32:16 +0200

    Create Informations.jsx

---
+++ src/components/Informations.jsx
@@ -0,0 +1,18 @@
+export function Informations() {
+  return (
+    <div>
+      Welcome to low_kb shell
+
+      {'\n\n'}> whoami
+      {'\n'}low_kb – JS, C++, VM, WebRTC
+
+      {'\n\n'}> github
+      {'\n'}https://github.com/lowkb
+
+      {'\n\n'}> projects
+      {'\n'}- lowScript
+      {'\n'}- WebRTC Chat
+      {'\n'}- Dashboard UI
+    </div>
+  )
+}

9c1182e6aed9b729fd680c68e4ff170ef9d010fa
low_kb <69186337+lowkb@users.noreply.github.com>
2025-06-28 04:09:07 +0200

    Update Layout.jsx

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,8 +1,13 @@
-
 export default function Layout({ children }) {
   return (
-    <div class="Layout">
+    <div class="layout-container">
+      <header>
+        <h1>My Portfolio</h1>
+      </header>
       <main>{children}</main>
+      <footer>
+        <small>© 2025 low_kb</small>
+      </footer>
     </div>
   );
 }

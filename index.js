f99b4644f337c4eaf5f2e8b7adb2d2430ff5c613
low_kb <69186337+lowkb@users.noreply.github.com>
2025-06-28 04:07:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { render } from 'preact';
-import Main from './src/Main.jsx';
+import Main from '@/Main.jsx';
+import '@style/style.css';
 
-render(<Main />, document.getElementById('root'));
+render(
+  <Main />, 
+  document.getElementById('root')
+);
